01b09de17e6de6cef1cbb93d82fc5ffb886fabb3
Eivind <56489286+dnivie@users.noreply.github.com>
2024-09-08 19:45:12 +0200

    testing file mount from host

--- kivy-docker/src/kivy_test.py
+++ kivy-docker/src/kivy_test.py
@@ -1,15 +1,22 @@
+import time
 import kivy
-kivy.require('2.1.0') # replace with your current kivy version !
+kivy.require('2.3.0') # replace with your current kivy version !
 
 
 from kivy.app import App
 from kivy.uix.label import Label
-
+file_path = "/tmp/testfile"
 
 class MyApp(App):
 
     def build(self):
         return Label(text='Hello world')
+
+    def read(self):
+        with open(file_path, 'r') as file:
+            data = file.read()
+        print(data)
+        time.sleep(0.5)
 
 
 if __name__ == '__main__':
